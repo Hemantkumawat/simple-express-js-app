e93b667c60987332ed2ef706918e372a5ccddac3
Hemant Kumawat <hemantkumawat275@gmail.com>
2019-06-30 00:12:45 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorsController = require('./controllers/errors');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,8 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+app.use(errorsController.err_404);
 
 app.listen(3000);
